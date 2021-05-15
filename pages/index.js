7f5031d2c19389a5a6cc4ad7fcf90b5ebfd265e1
Jacob Ford <j@cobford.com>
2021-05-15 19:45:01 -0400

    Basic feed fetching

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+import Parser from 'rss-parser'
+const parser = new Parser()
+
+export default function Home(props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -16,40 +19,9 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {props.feed.items.map(item =>
+          <a href={item.link}>{item.title}</a>
+        )}
       </main>
 
       <footer className={styles.footer}>
@@ -66,4 +38,17 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const feedUrl = 'https://thebrick.house/rss'
+  const feed = await parser.parseURL(feedUrl)
+  console.log(`Parsed feed: ${feed.title} with ${feed.items.length} items`)
+  console.log(`Example: ${Object.keys(feed.items[0])}`)
+  return {
+    props: {
+      inputUrl: feedUrl,
+      feed: feed,
+    }
+  }
 }
